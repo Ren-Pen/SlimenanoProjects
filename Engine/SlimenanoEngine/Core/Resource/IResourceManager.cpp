2036771d033375a4c9397c532a85b36f0c18ceeb
肖峥瑜 <xzy5487@outlook.com>
2025-09-03 06:50:28 +0900

    ResourceManager

---
+++ Engine/SlimenanoEngine/Core/Resource/IResourceManager.cpp
@@ -0,0 +1,24 @@
+/*
+Slimenano Engine
+    Copyright (C) 2025  zyu.xiao
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "../Base/Export.h"
+#include "../Base/TypesImpl.h"
+
+#include "IResourceManager.h"
+
+template SLIMENANO_CORE_API auto Slimenano::Core::Base::TypeId::Get<Slimenano::Core::Resource::IResourceManager>() -> const TypeId*;
---
+++ Engine/SlimenanoEngine/Core/Resource/IResourceManager.h
@@ -0,0 +1,40 @@
+/*
+Slimenano Engine
+    Copyright (C) 2025  zyu.xiao
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+#pragma once
+#ifndef SLIMENANO_PROJECT_ENGINE_CORE_RESOURCE_I_RESOURCE_MANAGER_H
+#    define SLIMENANO_PROJECT_ENGINE_CORE_RESOURCE_I_RESOURCE_MANAGER_H
+
+#    include "../Base/Status.h"
+#    include "../Base/Export.h"
+#    include "../Module/IBaseModule.h"
+
+namespace Slimenano::Core::Resource {
+
+using ResourceHandle = unsigned long long;
+
+class SLIMENANO_CORE_API IResourceManager : public Module::IBaseModule<IResourceManager> {
+  public:
+    ~IResourceManager() override = default;
+    virtual auto Open(const char* location, ResourceHandle* handle) -> Base::Status = 0;
+    virtual auto Close(const ResourceHandle* handle) -> Base::Status = 0;
+
+
+};
+}
+
+#endif
